64671475e6d498b39179c7f213259e6983add6ef
julienlucas <contact@julienlucas.com>
2022-10-12 21:00:14 +0200

    Update urls gateways with env vars

--- front/infrastructure/MessagesGateway.tsx
+++ front/infrastructure/MessagesGateway.tsx
@@ -4,7 +4,7 @@ import { Message } from '../domain/entities/message.interface';
 export default class MessagesGateway extends SingletonMixin() {
   async getMessages(realtorId: string): Promise<Message> {
     try {
-      const res = await fetch(`http://localhost:8080/realtors/${realtorId}/messages`);
+      const res = await fetch(`${process.env.PUBLIC_API_ENTRYPOINT!}${realtorId}/messages`);
       const data = await res.json();
 
       return data;
@@ -15,7 +15,7 @@ export default class MessagesGateway extends SingletonMixin() {
 
   async getSelectedMessage(realtorId: string, messageId: string): Promise<Message> {
     try {
-      const res = await fetch(`http://localhost:8080/realtors/${realtorId}/messages/${messageId}`);
+      const res = await fetch(`${process.env.PUBLIC_API_ENTRYPOINT!}${realtorId}/messages/${messageId}`);
       const data = await res.json();
 
       return data;
@@ -26,7 +26,7 @@ export default class MessagesGateway extends SingletonMixin() {
 
   setMessageStatusAsReaded(realtorId: string, messageId: string): void {
     try {
-      fetch(`http://localhost:8080/realtors/${realtorId}/messages/${messageId}`, {
+      fetch(`${process.env.PUBLIC_API_ENTRYPOINT!}${realtorId}/messages/${messageId}`, {
         method: "PATCH",
         body: JSON.stringify({
           read: true
--- front/infrastructure/RealtorsGateway.tsx
+++ front/infrastructure/RealtorsGateway.tsx
@@ -4,7 +4,7 @@ import { Realtor } from '../domain/entities/realtor.interface';
 export default class RealtorsGateway extends SingletonMixin() {
   async getRealtors (): Promise<Realtor> {
     try {
-      const res = await fetch(process.env.PUBLIC_API_ENTRYPOINT);
+      const res = await fetch(process.env.PUBLIC_API_ENTRYPOINT!);
       const data = await res.json();
 
       return data;
